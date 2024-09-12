5e7e8b84c03e1a09c097edf63ed401eb7446f088
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-09-12 15:42:30 +0200

    Resoved ESLint issues

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+import { Container, Row, Button } from "reactstrap";
+import { NavLink, Link } from "reactstrap";
+import logo from "../../assets/images/logo.png";
+
+const nav__links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "#",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex align-items-center justify-content-between">
+            {/*  ================LOGO ================== */}
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            {/*  ================LOGO end================== */}
+
+            {/*  ================menu start================== */}
+              <div className="navigation">
+                <ul className="menu d-flex align-items-center gap-5">
+                  
+                </ul>
+              </div>
+            {/*  ================menu end================== */}
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
