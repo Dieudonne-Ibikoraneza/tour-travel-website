a18d3edcbcd270e1288b40cecb38e73593917566
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-09-12 16:03:57 +0200

    Finished the navbar section

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 import { Container, Row, Button } from "reactstrap";
-import { NavLink, Link } from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
+
+import './header.css'
 
 const nav__links = [
   {
@@ -10,7 +12,7 @@ const nav__links = [
     display: "Home",
   },
   {
-    path: "#",
+    path: "/about",
     display: "About",
   },
   {
@@ -32,12 +34,31 @@ const Header = () => {
             {/*  ================LOGO end================== */}
 
             {/*  ================menu start================== */}
-              <div className="navigation">
-                <ul className="menu d-flex align-items-center gap-5">
-                  
-                </ul>
-              </div>
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {nav__links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink to={item.path} className={navClass => navClass.isActive ? 'active__link' : ''}>{item.display}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
             {/*  ================menu end================== */}
+
+            <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nav__btn d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+
+              <span className="mobile__menu">
+                <i className="ri-menu-line"></i>
+              </span>
+            </div>
           </div>
         </Row>
       </Container>
